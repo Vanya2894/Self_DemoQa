e6c83001b72d798e5c35b92f8dfe9b3b76f539b3
Khlypalo_Ivan <vanya-khlypalo@yandex.ru>
2023-11-03 20:02:13 +0300

    first 03/11/2023

---
+++ tests/test_check_icon_demoqa.py
@@ -0,0 +1,20 @@
+from pages.demo_qa import DemoQa
+def test_icon_exist(browser):
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    demo_qa_page.click_on_the_icon()
+    assert demo_qa_page.equal_url()
+    assert demo_qa_page.exist_icon()
+
+
+
+
+    # browser.get('https://demoqa.com/')
+    # icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
+    #
+    # if icon is None:
+    #     print('Элемент не найден')
+    # else:
+    #     print('Элемент найден')
+
+
